4a7d44528ed2aa710a1135aa32be756d1f5f1846
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-18 21:17:30 -0500

    feat: support "ref" prop in Scenic

--- src/Scenic.tsx
+++ src/Scenic.tsx
@@ -7,17 +7,20 @@ export interface ScenicProps {
   initialPath: string
 }
 
-export const Scenic = (props: ScenicProps) => {
-  const scenic = useMemo(() => new ScenicRoot(), [])
-  useEffect(() => {
-    scenic.visit(props.initialPath || '/')
-  }, [])
+export const Scenic = React.forwardRef(
+  (props: ScenicProps, ref: React.Ref<ScenicRoot>) => {
+    const scenic = useMemo(() => new ScenicRoot(), [])
+    React.useImperativeHandle(ref, () => scenic, [])
+    useEffect(() => {
+      scenic.visit(props.initialPath || '/')
+    }, [])
 
-  const { Provider } = ScenicRoot.Context
-  return (
-    <Provider value={scenic}>
-      {props.children}
-      {/** TODO: render the stack here */}
-    </Provider>
-  )
-}
+    const { Provider } = ScenicRoot.Context
+    return (
+      <Provider value={scenic}>
+        {props.children}
+        {/** TODO: render the stack here */}
+      </Provider>
+    )
+  }
+)
