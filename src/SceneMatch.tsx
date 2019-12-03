85de76ef920d3242c3763a2ab7a9d5dfad674a33
Alec Larson <alec.stanford.larson@gmail.com>
2019-12-03 17:59:56 -0500

    feat: add "match" prop to SceneMatch component
    The "force" prop is now the "match" prop, which can be either a boolean or boolean-returning function.
    
    Match functions are observed by the SceneMatch component.

--- src/SceneMatch.tsx
+++ src/SceneMatch.tsx
@@ -1,19 +1,28 @@
+import is from '@alloc/is'
 import React, { ReactNode } from 'react'
 import { withAuto } from 'wana'
 import { Scene } from './Scene'
 import { useScene } from './useScene'
 
 type Props = ({ path: string } | { scene: Scene }) & {
-  force?: boolean
+  match?: boolean | ((scene: Scene) => boolean)
   children?: ReactNode
 }
+
+const isMounted = (scene: Scene) => scene.isMounted
+const { Provider: SceneProvider } = Scene.Context
 
 /** Mount children only when the relevant scene is mounted. */
 export const SceneMatch = withAuto((props: Props) => {
   const scene = 'scene' in props ? props.scene : useScene(props.path)
-  if (props.force || scene.isMounted) {
-    const { Provider } = Scene.Context
-    return <Provider value={scene}>{props.children}</Provider>
+
+  const match = is.boolean(props.match)
+    ? props.match
+    : (props.match || isMounted)(scene)
+
+  if (match) {
+    return <SceneProvider value={scene}>{props.children}</SceneProvider>
   }
+
   return null
 })
