ddc9f456986ab4647db2008726256230e3c83a21
Alec Larson <alec.stanford.larson@gmail.com>
2020-01-23 11:47:10 -0500

    feat: add "onMatch" prop to SceneMatch

--- src/SceneMatch.tsx
+++ src/SceneMatch.tsx
@@ -1,11 +1,12 @@
 import { is } from '@alloc/is'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
 import { withAuto } from 'wana'
 import { Scene } from './Scene'
 import { useScene } from './useScene'
 
 type Props = ({ path: string } | { scene: Scene }) & {
   match?: boolean | ((scene: Scene) => boolean)
+  onMatch?: (scene: Scene) => void
   children?: ReactNode
 }
 
@@ -23,6 +24,12 @@ export const SceneMatch = withAuto((props: Props) => {
   const match = is.boolean(props.match)
     ? props.match
     : (props.match || isMounted)(scene)
+
+  useEffect(() => {
+    if (props.onMatch && match) {
+      props.onMatch(scene)
+    }
+  }, [scene, match])
 
   if (match) {
     return <SceneProvider value={scene}>{props.children}</SceneProvider>
