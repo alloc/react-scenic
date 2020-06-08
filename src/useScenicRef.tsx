31c5ecbdb50c63d5a1528d90f3bf80b7e5f1ffdb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-06-08 19:41:36 -0400

    feat: export `ScenicRef` type

--- src/useScenicRef.tsx
+++ src/useScenicRef.tsx
@@ -5,3 +5,5 @@ import { ScenicRoot } from './ScenicRoot'
  * Use the returned object as the `ref` prop of a `Scenic` element.
  */
 export const useScenicRef = () => useRef<ScenicRoot>(null)
+
+export type ScenicRef = ReturnType<typeof useScenicRef>
